f4cfa3e61ab569309bb44ed362fabc82f24a3618
koreanhole <koreanhole1@gmail.com>
2021-03-13 00:50:35 +0900

    initial commit

---
+++ txt2att.py
@@ -0,0 +1,49 @@
+import pandas as pd
+
+print('출석 txt 파일 제목 예시: att-210311.txt')
+print('출석 txt 파일 경로: 월/일/att-21{월}{일}.txt')
+print('*******************')
+print('채팅 txt 파일 제목 예시: chat-210311.txt')
+print('채팅 txt 파일 경로: 월/일/chat-21{월}{일}.txt')
+print('*******************')
+
+month = input('월을 입력하세요(ex. 03): ')
+day = input('일을 입력하세요(ex. 11): ')
+
+studentDict = {}
+
+# 출석한 학생 확인
+f = open(f'/Users/soonhyeong/Documents/Developing/2021-capstone-assistant/{month}/{day}/att-21{month}{day}.txt', 'r')
+attendanceList = f.readlines()
+f.close()
+
+for attendance in attendanceList:
+    student = attendance.split()
+    studentName = student[1].strip()
+    studentDict[studentName] = 0
+
+# 지각한 학생 확인
+f = open(f'/Users/soonhyeong/Documents/Developing/2021-capstone-assistant/{month}/{day}/chat-21{month}{day}.txt', 'r')
+lateChatList = f.readlines()
+f.close()
+
+for chat in lateChatList:
+    index = chat.find('ATT2')
+    # 각 채팅 줄마다 ATT2라는 단어가 있다면
+    if index != -1:
+        student = chat[index:].split()
+        studentName = student[1].strip()
+        # 교수님 이름은 건너뛰기
+        if studentName == '유하진':
+            continue
+
+        # 만약 이미 출석한 학생이라면
+        if studentDict.get(studentName) != None:
+            print(f'student name "{studentName}" duplicate')
+        else:
+            studentDict[studentName] = 1
+
+# 새로운 학생 출석 데이터프레임 생성
+df = pd.DataFrame(list(studentDict.items()), columns=['name', 'ATT'])
+df.to_excel(f'./{month}/{day}/att-result-21{month}{day}.xlsx', index=False)
+
